96f77e53804d7810f684f592f013421ed208abef
Leonel Sosa <leo.vm.cba@gmail.com>
2022-03-14 21:47:21 -0300

    Added functionality to quote cryptocurrencies via the API and added a spinner after the query

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 const criptomonedasSelect = document.querySelector('#criptomonedas');
 const monedasSelect = document.querySelector('#moneda');
 const form = document.querySelector('#formulario');
+const resultado = document.querySelector('#resultado');
+
+
 
 const objBusqueda = {
     moneda: '',
@@ -9,7 +12,7 @@ const objBusqueda = {
 
 document.addEventListener('DOMContentLoaded', () => {
     consultatCriptomonedas();
-    
+
     form.addEventListener('submit', submitForm);
     criptomonedasSelect.addEventListener('change', leerValor);
     monedasSelect.addEventListener('change', leerValor);
@@ -55,8 +58,11 @@ function submitForm(e) {
         mostrarAlerta('Ambos campos son obligatorios');
         return
     }
+
+    consultarAPI();
 }
 
+//Alerta de error
 function mostrarAlerta(mensaje){
     const existeError = document.querySelector('.error');
 
@@ -73,3 +79,68 @@ function mostrarAlerta(mensaje){
     }
 }
 
+function consultarAPI() {
+    const { moneda, criptomoneda } = objBusqueda;
+
+    const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
+
+    mostrarSpinner();
+
+    setTimeout(() => {
+        fetch(url)
+            .then(respuesta => respuesta.json())
+            .then(resultado => {
+            mostrarCotizacionHTML(resultado.DISPLAY[criptomoneda][moneda]);
+        })
+    }, 3500);
+
+}
+
+function mostrarSpinner() {
+    limpiarHTML();
+
+    const spinner = document.createElement('div');
+    spinner.classList.add('spinner')
+    spinner.innerHTML = `
+        <div class="cube1"></div>
+        <div class="cube2"></div>
+    `
+
+    resultado.appendChild(spinner);
+}
+
+function mostrarCotizacionHTML(cotizacion) {
+    limpiarHTML();
+
+    const criptoM = criptomonedasSelect.options[criptomonedasSelect.selectedIndex].text;
+
+    const { CHANGEPCT24HOUR, HIGHDAY, LOWDAY, LASTUPDATE, PRICE } = cotizacion;
+
+    const precio = document.createElement('p');
+    precio.classList.add('precio');
+    precio.innerHTML = `Un ${criptoM} es igual a: <br><span>$${PRICE}</span>`;
+
+    const precioAlto = document.createElement('p');
+    precioAlto.innerHTML = `El precio mas alto del dia es: <span>${HIGHDAY}</span>`;
+
+    const precioBajo = document.createElement('p');
+    precioBajo.innerHTML = `El precio más bajo del dia es: <span>${LOWDAY}</span>`;
+
+    const variacion = document.createElement('p');
+    variacion.innerHTML = `Variación últimas 24hs: <span>${CHANGEPCT24HOUR}%</span>`;
+
+    const actualizacion = document.createElement('p');
+    actualizacion.innerHTML = `Ultima actualización: <span>${LASTUPDATE}</span>`;
+
+    resultado.appendChild(precio);
+    resultado.appendChild(precioAlto);
+    resultado.appendChild(precioBajo);
+    resultado.appendChild(variacion);
+    resultado.appendChild(actualizacion);
+}
+
+function limpiarHTML(){
+    while(resultado.firstChild){
+        resultado.removeChild(resultado.firstChild);
+    }
+}
